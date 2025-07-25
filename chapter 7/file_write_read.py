274cdf0ad856965a2e92b164faa9c44cf5e68cce
cooperear <windsoft23@gmail.com>
2025-07-26 02:19:51 +0900

    asdsdada

--- chapter 7/file_write_read.py
+++ chapter 7/file_write_read.py
@@ -1,6 +1,6 @@
 with open ('./test_text.txt', 'w',encoding='utf-8')as f:
     f.write('안뇽하세용 \n')
-    f.write('박시훈이에요 \n')
+    f.write('cooperear이에요 \n')
     f.write('좋은 날씨에요 \n')
     f.write('파이썬 연습하고 있어요\n')
 
